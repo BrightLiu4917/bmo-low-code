d52bcf4c3310b88a6cbf185cd33d792e45a793ca
liuweiliang <791771832@qq.com>
2025-10-24 14:52:10 +0800

    feat: README.MD

--- route/innerapi/v2.php
+++ route/innerapi/v2.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use BrightLiu\LowCode\Controllers\LowCode\InitOrgDiseaseController;
 
 Route::prefix('v2')->group(function() {
-    Route::get('init/org-disease', [InitOrgDiseaseController::class])
+    Route::get('init/org-disease', [InitOrgDiseaseController::class,'__invoke'])
     ->middleware('bmp.disease.auth.inner');
 });
 
