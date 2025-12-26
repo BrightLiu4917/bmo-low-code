6ab4615f5b12c3ff6b63fe66fbedc6b41641d775
liuweiliang <7349846+liuweiliang@user.noreply.gitee.com>
2025-12-26 11:42:58 +0800

    fix: 中间件新增 当前登录账号未关联职工信息， 请在资源中心职工管理新增当前职工信息

--- src/Middleware/DiseaseAuthenticate.php
+++ src/Middleware/DiseaseAuthenticate.php
@@ -59,6 +59,12 @@ class DiseaseAuthenticate
                 throw new AuthenticateException('BmoAuth Account invalid.');
             }
 
+
+            $rcUserId = data_get($bmoAccount, 'rc_user_id','');
+            if (empty($rcUserId)){
+                throw new AuthenticateException('当前登录账号未关联职工信息， 请在资源中心职工管理新增当前职工信息');
+            }
+
             if (empty($bmoAccountDataPermission = BmoAuthApiService::instance()->getArcDataPermissionByUserId(userId: (int)$bmoAccount['id'],arcCode: $arcCode))){
                 throw new AuthenticateException('BmoAuth Account Data Permission invalid.');
             }
